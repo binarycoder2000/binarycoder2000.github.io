08adebb6191b7305942ed58b6dcbc2175965c875
BinaryCoder2000 <33234085+binarycoder2000@users.noreply.github.com>
2017-10-30 18:23:36 -0700

    Add files via upload

---
+++ encoderoutput.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+<link href='https://fonts.googleapis.com/css?family=Baloo' rel='stylesheet'>
+</head>
+
+<style>
+html{
+	font-family: 'Baloo';font-size: 22px;
+}
+</style>
+
+<body>
+
+<h2>Output:<h2>
+<?php echo $_GET["encoderinput"]; ?><br>
+
+</body>
+</html>
